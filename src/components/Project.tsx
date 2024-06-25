bcdda52be680894d812528f65ef80953bfb672c1
Krishnamurthy R <krishnamurthy.ravi@bounteous.com>
2024-06-25 11:21:21 +0530

    added animations

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { skills } from '../skill';
+import React from "react";
+import { skills } from "../skill";
 
 interface Project {
   name: string;
@@ -15,13 +15,21 @@ interface Props {
 
 const ProjectComponent: React.FC<Props> = ({ project }) => {
   return (
-    <div className="flex-1 font-mono text-white flex gap-4 flex-col md:w-[480px] w-[340px]">
-      <img className="w-[480px] rounded-xl" src={project.src} alt={project.name} />
+    <div className="flex-1 font-mono text-white flex gap-4 flex-col md:w-[480px] w-[340px] pb-8">
+      <img
+        className="w-[480px] rounded-xl"
+        src={project.src}
+        alt={project.name}
+      />
       <h2 className="text-2xl">{project.name}</h2>
-      <p className='flex flex-wrap'>{project.description}</p>
+      <p className="flex flex-wrap">{project.description}</p>
       <div className="flex flex-wrap">
         {project.stack.map((skill, index) => (
-          <img key={index} src={skills[skill]} alt={skill} />
+          <img
+            key={index}
+            src={skills[skill]}
+            alt={skill}
+          />
         ))}
       </div>
     </div>
